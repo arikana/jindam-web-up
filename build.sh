80fb7951f9a0842f60e448655da4504e1aac2432
arikana <moonjb104605@gmail.com>
2025-12-06 00:05:14 +0900

    theme 디렉토리 수정

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd ./jindam_site/
-hugo build --themesDir=./theme/ --ignoreCache --destination ../docs/
+hugo build --themesDir=./themes/ --ignoreCache --destination ../docs/
 
 echo "build complete"
